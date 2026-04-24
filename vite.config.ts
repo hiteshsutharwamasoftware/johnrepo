fcc81a2c67f0be6a1248475e98a0f52d30f31d8e
Hitesh suthar <samier.sompura@gmail.com>
2026-04-24 11:55:02 +0530

    url change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0','99c5-2409-40d4-1247-3176-ccd0-a379-43aa-e509.ngrok-free.app'],
+    allowedHosts: true,
   },
 })
